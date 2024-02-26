925cc52653a6e89dd312aa93a7d82538ca228bb5
Victor Hugo <victor.hugo.dcg@gmail.com>
2024-02-25 22:31:11 -0500

    feat: working desktop application for mebots

---
+++ main.js
@@ -0,0 +1,38 @@
+const { app, BrowserWindow } = require('electron');
+const path = require('path');
+
+function createWindow () {
+  const win = new BrowserWindow({
+    width: 1000,
+    height: 800,
+    webPreferences: {
+      preload: path.join(__dirname, 'preload.js'),
+      icon: path.join(__dirname, 'images/logo/logo.png'),
+      partition: 'persist:infragistics',
+    },
+  });
+
+  // Load webpage
+  win.loadURL(
+      'https://mebots.io/',
+      {
+          userAgent: 'Chrome',
+      },
+  );
+}
+
+app.whenReady().then(() => {
+  createWindow();
+
+  app.on('activate', () => {
+    if (BrowserWindow.getAllWindows().length === 0) {
+      createWindow()
+    }
+  })
+})
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit()
+  }
+})
